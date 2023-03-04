65c5a48cf6a17ed3ebab450e8bfe25e4fd568e9f
iohcrepoleved <aram829@icloud.com>
2023-03-04 17:21:13 +0900

    Initial Commit

---
+++ SeoulBikeMap/SeoulBikeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeoulBikeMapApp.swift
+//  SeoulBikeMap
+//
+//  Created by 최아람 on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct SeoulBikeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
